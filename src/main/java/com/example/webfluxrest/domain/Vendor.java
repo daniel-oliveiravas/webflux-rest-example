29970263e016bff963d7bd4aa1133fe9618aa318
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2018-04-18 22:58:28 -0300

    Add Builder annotation to the domain classes

--- src/main/java/com/example/webfluxrest/domain/Vendor.java
+++ src/main/java/com/example/webfluxrest/domain/Vendor.java
@@ -1,11 +1,13 @@
 package com.example.webfluxrest.domain;
 
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
+@Builder
 public class Vendor {
 
     @Id
