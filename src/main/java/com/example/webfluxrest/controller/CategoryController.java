a4f967db8605093e9b16b94a5b48cc4125925119
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2018-04-18 23:26:14 -0300

    Add category controller

---
+++ src/main/java/com/example/webfluxrest/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package com.example.webfluxrest.controller;
+
+import com.example.webfluxrest.domain.Category;
+import com.example.webfluxrest.repository.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    Flux<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Mono<Category> getById(@PathVariable String id) {
+        return categoryRepository.findById(id);
+    }
+}
