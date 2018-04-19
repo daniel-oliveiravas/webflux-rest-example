bb87571c492993c9acb539b692bc95625460f1c7
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2018-04-18 22:48:03 -0300

    Add attributes to Vendor domain

--- src/main/java/com/example/webfluxrest/domain/Vendor.java
+++ src/main/java/com/example/webfluxrest/domain/Vendor.java
@@ -1,4 +1,16 @@
 package com.example.webfluxrest.domain;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
 public class Vendor {
+
+    @Id
+    private String Id;
+
+    private String firstName;
+    private String lastName;
 }
