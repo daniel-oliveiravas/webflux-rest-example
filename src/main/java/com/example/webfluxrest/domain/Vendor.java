0b406fc5318113c521405070eff5809b7b255545
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2018-04-21 18:36:32 -0300

    Fix variable name

--- src/main/java/com/example/webfluxrest/domain/Vendor.java
+++ src/main/java/com/example/webfluxrest/domain/Vendor.java
@@ -15,7 +15,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Vendor {
 
     @Id
-    private String Id;
+    private String id;
 
     private String firstName;
     private String lastName;
